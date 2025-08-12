861d170de164027d5f083179fd45568c71fb6471
Jertup <jere.tupp@gmail.com>
2025-08-12 09:42:50 +0300

    Add Fallback route for app

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -78,6 +78,11 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
+// Fallback handler: send back React's index.html file for any non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
