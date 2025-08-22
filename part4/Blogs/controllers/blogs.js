a7ac17b567dc1d1ec1c504e23403d299cabf01b2
Jertup <jere.tupp@gmail.com>
2025-08-22 11:30:23 +0300

    Add supertests to Blogs app

--- part4/Blogs/controllers/blogs.js
+++ part4/Blogs/controllers/blogs.js
@@ -1,18 +1,68 @@
 const blogsRouter = require('express').Router()
+const { request } = require('../app')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      response.status(400).json({ error: error.message })
+    } else {
+      response.status(500).json({ error: 'Something went wrong' })
+    }
+  }
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+      return response.status(404).end()
+    }
+    response.json(blog) // No manual transformation needed
+  } catch (error) {
+    if (error.name === 'CastError') {
+      response.status(400).json({ error: 'malformatted id' })
+    } else {
+      response.status(500).json({ error: 'internal server error' })
+    }
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, url, likes },
+      { new: true, runValidators: true }
+    )
+    if (!updatedBlog) {
+      return response.status(404).end()
+    }
+    response.json(updatedBlog)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
